853b561539735ade728389bea906ffc84aad1d2e
Yoel Melki <ymelki@gmail.com>
2022-09-19 11:04:36 +0200

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// affiche dans la console le texte entre guillemet !
 console.log("je test le JS ! ")
+
+/* 
+1. LES VARIABLES
+ - Les nombres , 
+ - Les chaines de caracteres,
+ - Les booleens
+ - Les tableau
+
+2. LES CONDITIONS
+3. LES BOUCLES
+4. LES FONCTIONS
+*/
+
+
+// les variables
+prenom="Yoel"; // chaine de caractere / string
+age = 35; // Numérique : sans guillemet
+
+console.log("Je m'appelle " + prenom + ". et j'ai " + age + " ans.");
+
+// concatenations
+
